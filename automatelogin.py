fadacdd3b896b49b98fad948f7a91a03453ae2a9
Ashish Malik <44671044+ashish493@users.noreply.github.com>
2018-12-14 10:19:41 +0530

    Script for automatic login on any website

--- automatelogin.py
+++ automatelogin.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python
+#imports specific drivers from module
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+#Takes input from user
+user = input("Enter your UserId")
+pass = input("Enter your Password")
+url = input("Enter the address of site you want to use!!!")
+button = input("Enter the name of button to be clicked.")
+# If using Chrome!!
+driver = webdriver.Chrome()
 
-driver=webdriver.Chrome()
 def site():
-   driver.get("http://172.16.1.11:1000/keepalive?0c0f0c0604030127")
-   driver.find_element_by_id("login").send_keys("B218***")
-   driver.find_element_by_id("pass").send_keys("***************")
-   driver.find_element_by_id("Continue").click()
+   driver.get(url)
+   driver.find_element_by_id("login").send_keys(user)
+   driver.find_element_by_id("pass").send_keys(pass)
+   driver.find_element_by_id(button).click()
